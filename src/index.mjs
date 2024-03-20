c9c9fd8590e0bded0abb6de3b84e62ed95dc25cc
bryanvengwa <bmvengwa@gmail.com>
2024-03-20 04:14:34 +0200

    added cookie parser middleware

--- src/index.mjs
+++ src/index.mjs
@@ -6,21 +6,21 @@ const app = express();
 
 const loggingMiddleware = function (req, res, next) {
   // console.log(`${req.method} - ${req.url} `);
-  
+
   next();
 };
 
 app.use(express.json());
 app.use(loggingMiddleware);
+app.use(cookieParser());
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
-  console.log(request.headers.cookie)
-  response.cookie('hello', 'world', {maxAge:60000 * 60 * 60});
+  console.log(request.headers.cookie);
+  response.cookie('hello', 'world', { maxAge: 60000 * 60 * 60 });
   return response.status(201).send({ msg: 'Hello world!' });
-  
 });
 
 app.listen(PORT, () => {
