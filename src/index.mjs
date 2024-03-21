b01429c5482008b93408e2888b66913cdf55c338
bryanvengwa <bmvengwa@gmail.com>
2024-03-21 09:23:03 +0200

    added visited property to the session

--- src/index.mjs
+++ src/index.mjs
@@ -14,14 +14,25 @@ const loggingMiddleware = function (req, res, next) {
 app.use(express.json());
 app.use(loggingMiddleware);
 app.use(cookieParser('helloworld'));
+app.use(session({
+  secret: 'bryanvengwa',
+  // this is ideal to be saved to false
+  saveUninitialized:false,
+  resave:false,
+  cookie:{
+    maxAge:60000 * 60 * 60 * 24 
+  }
+}));
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
   response.cookie('hello', 'world', { maxAge: 60000 * 60 * 60, signed: true });
-  console.log(request.headers.cookie);
-  console.log(request.cookies);
+  console.log(request.session)
+  console.log(request.sessionID)
+  console.log(request.session.id)
+  request.session.visited = true;
 
   return response.status(201).send({ msg: 'Hello world!' });
 });
