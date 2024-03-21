25efff216171fb6008ef9dbb20a5311dbe4dc933
bryanvengwa <bmvengwa@gmail.com>
2024-03-21 12:09:56 +0200

    done did a auth strategy

--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -1,1 +1,17 @@
-import passport from "passport";
+import passport from 'passport';
+import { Strategy } from 'passport-local';
+import { mockUsers } from '../utils/constants.mjs';
+
+passport.use(
+  new Strategy((userName, password, done) => {
+    try {
+      const findUser = mockUsers.find((user) => userName === userName);
+      if (!findUser) throw new Error('User not found');
+      if (findUser.password !== password)
+        throw new Error('Invalid Credentials');
+      done(findUser, null);
+    } catch (error) {
+      done(error, null);
+    }
+  })
+);
