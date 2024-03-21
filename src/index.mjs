b1c830816dfd8ec3eb3ebf3a9627798fe017ec71
bryanvengwa <bmvengwa@gmail.com>
2024-03-21 04:14:56 +0200

    express sessions

--- src/index.mjs
+++ src/index.mjs
@@ -12,14 +12,16 @@ const loggingMiddleware = function (req, res, next) {
 
 app.use(express.json());
 app.use(loggingMiddleware);
-app.use(cookieParser());
+app.use(cookieParser('helloworld'));
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
+  response.cookie('hello', 'world', { maxAge: 60000 * 60 * 60, signed: true });
   console.log(request.headers.cookie);
-  response.cookie('hello', 'world', { maxAge: 60000 * 60 * 60 });
+  console.log(request.cookies);
+
   return response.status(201).send({ msg: 'Hello world!' });
 });
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -5,8 +5,13 @@ const router = Router();
 
 
 router.get('/api/products', (request, response) => {
-  
-  return  response.send([{ id: 1, name: 'chicken breast' }]);
+  console.log(request.headers.cookie);
+  console.log(request.cookies)
+  if(request.signedCookies.hello && request.signedCookies.hello === 'world'){
+
+    return  response.send([{ id: 1, name: 'chicken breast' ,}]);
+  }
+  return response.send('Sorry you need the correct cookie')
   });
   
 export default router;
